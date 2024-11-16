0c77d11f8021fd83a83e172afa9c6010bccb3f04
Pratik228 <pratikmishra79@gmail.com>
2024-11-16 19:57:07 +0530

    indexing of the user

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -54,6 +54,8 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
+userSchema.index({ email: 1 });
+
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
   this.password = await bcrypt.hash(this.password, 12);
